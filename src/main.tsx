0faecfe10f63fbba4f3daca67d6e4d1e866ca122
Bastien Sun <43788700+bastiensun@users.noreply.github.com>
2023-10-30 20:54:16 +0100

    fix: remove "/prettier" base path

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     element: <App />,
-    path: "/prettier",
+    path: "/",
   },
 ]);
 
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/prettier",
   plugins: [react()],
   resolve: {
     alias: {
